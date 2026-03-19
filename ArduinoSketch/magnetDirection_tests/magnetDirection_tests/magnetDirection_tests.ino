57d0de8f93f19bc8510ea1e1b7d909ae93311516
Daniel Almeida Chagas <daniel.helptech@gmail.com>
2026-03-19 20:19:06 -0300

    Create magnetDirection_tests.ino

---
+++ ArduinoSketch/magnetDirection_tests/magnetDirection_tests/magnetDirection_tests.ino
@@ -0,0 +1,40 @@
+/*
+===============================================================================================================
+QMC5883LCompass.h Library Direction Example Sketch
+Learn more at [https://github.com/mprograms/QMC5883Compas]
+
+This example shows how to get Compass direction.
+
+===============================================================================================================
+Release under the GNU General Public License v3
+[https://www.gnu.org/licenses/gpl-3.0.en.html]
+===============================================================================================================
+*/
+#include <QMC5883LCompass.h>
+
+QMC5883LCompass compass;
+
+void setup() {
+  Serial.begin(115200);
+  compass.setADDR(0x0D);
+  compass.init();
+}
+
+void loop() {
+  compass.read();
+  
+  byte a = compass.getAzimuth();
+
+  char myArray[3];
+  compass.getDirection(myArray, a);
+
+  Serial.print("A: ");
+  Serial.print(a);
+  Serial.print(" - ");
+  //Serial.print(myArray[0]);
+  Serial.print(myArray[1]);
+  Serial.print(myArray[2]);
+  Serial.println();
+  
+  delay(250);
+}
